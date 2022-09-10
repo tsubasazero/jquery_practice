ab755b9f49a55e0a63d5e9cfbd8259c52a07ed47
tsubasazero <tsubasaito24@gmail.com>
2022-09-10 16:31:23 +0900

    question

--- q8/sample/js/app.js
+++ q8/sample/js/app.js
@@ -1,4 +1,6 @@
 var $jscomp = $jscomp || {}; $jscomp.scope = {}; $jscomp.createTemplateTagFirstArg = function (a) { return a.raw = a }; $jscomp.createTemplateTagFirstArgWithRaw = function (a, d) { a.raw = d; return a };
+
+
 $(function () {
   function a(e) {
     $(".message").remove(); var b; 0 < (null == (b = e[0].items) ? void 0 : b.length) ? $.each(e[0].items, function (h, c) {
@@ -13,5 +15,15 @@ $(function () {
       $(".lists").empty(); $(".message").remove(); 0 === b.status ? $(".lists").before('<div class="message">\u6b63\u5e38\u306b\u901a\u4fe1\u3067\u304d\u307e\u305b\u3093\u3067\u3057\u305f\u3002<br>\u30a4\u30f3\u30bf\u30fc\u30cd\u30c3\u30c8\u306e\u63a5\u7d9a\u306e\u78ba\u8a8d\u3092\u3057\u3066\u304f\u3060\u3055\u3044\u3002</div>') : 400 === b.status ? $(".lists").before('<div class="message">\u691c\u7d22\u30ad\u30fc\u30ef\u30fc\u30c9\u304c\u6709\u52b9\u3067\u306f\u3042\u308a\u307e\u305b\u3093\u3002<br>1\u6587\u5b57\u4ee5\u4e0a\u3067\u691c\u7d22\u3057\u3066\u4e0b\u3055\u3044\u3002</div>') :
         $(".lists").before('<div class="message">\u4e88\u671f\u305b\u306c\u30a8\u30e9\u30fc\u304c\u8d77\u304d\u307e\u3057\u305f\u3002<br>\u518d\u8aad\u307f\u8fbc\u307f\u3092\u884c\u3063\u3066\u304f\u3060\u3055\u3044\u3002</div>')
     })
-  }); $(".reset-btn").on("click", function () { d = 1; f = ""; $(".lists").empty(); $(".message").remove(); $("#search-input").val("") })
+  }); $(".reset-btn").on("click", function () { d = 1; f = ""; $(".lists").empty(); $(".message").remove(); $("#search-input").val("")
+
+
+})
+
+  
+  
+  
+  
+  
+
 });
--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -2,65 +2,78 @@ $(function(){
   $(".search-btn").on("click",function(){
     const searchWord = $("#search-input").val();
     const pageCount = 1
-    // console.log(pageCount);
+
     const settings = {
         "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
         "method": "GET",
       }
-
       console.log(settings);
     // タイトル	title	可（AND）	部分一致テキスト検索
     // 出力フォーマット	format	不可	
     // atom:ATOM形式（デフォルト）、rss: RSS形式、 json: JSON-LD形式、html:HTML形式
     // 1ページあたりの結果数	count	不可	デフォルト20
-    
 
+    // (title["title"])
+    // (nameing["dc:creator"])
+    // (publisher["dc:publisher"])
 
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function (response, items) {
       const result = response['@graph'];
       console.log(result);
+
       
-      $(".lists").text(result);
+      let html ;
+      for(let i = 0; i<items.length; i++){
+      html += `
+      <li class="lists-item">
+        <div class="list-inner">
+          <p>タイトル：${result[0].items[0].title}</p>
+          <p>著者：${result[0].items[0]["dc:creator"]}</p>
+          <p>出版社：${result[0].items[0]["dc:publisher"]}</p>
+          <a target ="_blank" href="${result[0].items[0].link["@id"]}"}>書籍情報</a>
+        </div>
+      </li>`
 
-    //   console.log(response);
-    //   displayResult = function (){
-    //   $(".inner").text();
-    // }
-      // displayResult(result);
-     
-    
-    })
-    .fail(function (err) {
-      // const box = $(err).html("検索結果が見つかりませんでした。別のキーワードで検索して下さい。");
-      // err = [jqXHR, textStatus, errorThrown]
-      // console.log(box);
-      // $(function displayError(){
-      //   $(".inner").text();
-      alert('ファイルの取得に失敗しました。');
-      console.log(err)
-    })
+      }
+      // if(result.readyState == 4){
+      //   if(response.status === 200){
+         
+      //     alert("成功")
+      //   } else{
+      //     $(".lists").html('<li class="lists-box">');
+          $(".lists").empty().append(html);
+      //     alert("通信成功の失敗")
+ 
+      //   }
+      // }
 
-     
-    // });
-  })
+      // console.log(response);
 
-  $(".reset-btn").on("click",function(){
-    $("#search-input").val("");
-    $(".lists").reset();
-    // $(".lists").show();
-    // alert("リセット")
-  })
+    }).fail(function (err) {
+      
+      // displayError(err)
+      // alert("失敗")
+    });
+
+    // $(".reset-btn").on("click",function(){
+    //   $("#search-input").val("");
+    //   $(".lists").reset();
+    //   $(".lists").show();
+    //   alert("リセット")
+    // })
+
+  });
 });
 
 
-// API
-// const settings = {
-//   "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
-//   "method": "GET",
-// }
-// $.ajax(settings).done(function (response) {
-//   const result = response['@graph'];
-//   displayResult(result)
-// }).fail(function (err) {
-//   displayError(err)
-// });
+// // API
+// // const settings = {
+// //   "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
+// //   "method": "GET",
+// // }
+// // $.ajax(settings).done(function (response) {
+// //   const result = response['@graph'];
+// //   displayResult(result)
+// // }).fail(function (err) {
+// //   displayError(err)
+// // });
